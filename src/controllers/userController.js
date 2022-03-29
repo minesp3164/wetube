bb9ee95c4b70a25248ea7021bd9a5aa7d791060b
minesp3164 <lhd000721@naver.com>
2022-03-29 22:26:22 +0900

    1
    1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,12 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 
-export const getJoin = (req, res) => res.render("Join", { pageTitle: "Create Accounts" });
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Create Accounts" });
 export const postJoin = async(req, res) => {
     const { name, username, email, password,password2, location } = req.body;
+   
     if (password !== password2) {
-        return res.status(400).render("join.",
+        return res.status(400).render("join",
             {
                 pageTitle: "Join",
                 errorMessage: "Password confirmation does not match"
@@ -13,24 +14,24 @@ export const postJoin = async(req, res) => {
     }
     const exists = await User.exists({ $or: [{ username }, { email }] });
     if (exists) {
-        return res.status(400).render("join.",
-            {
+        return res.status(400).render("join",
+            {   
                 pageTitle: "Join",
                 errorMessage: "This username/email is already taken"
             });
     }
     try {
-        await User.create(
+        await User.create({
             name,
             username,
             email,
             password,
             location,
-        );
-        return res.redirect("/")
+        });
+        return res.redirect("/login")
     }
     catch (error) {
-        return res.redirect("/login");
+        return res.redirect("/");
     }
 };
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" })
